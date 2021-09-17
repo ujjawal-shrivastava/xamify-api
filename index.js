25586cda7147132574e20c36dc41210501ee193c
Ujjawal Shrivastava <ujjawalshrivastava19@gmail.com>
2021-09-17 14:28:14 +0530

    Updated Ping Route

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ app.use(express.json());
 // routes
 app.use("/api", require("./routes"));
 
-app.use("/ping", async(req, res) => {
-    res.send("Pong");
-});
-
 // error handler
 app.use(async(err, req, res, next) => {
     if (res.headersSent) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const api = require("express").Router();
 
+api.use("/ping", async(req, res) => {
+    res.send("Pong");
+});
+
 api.use("/auth", require("./auth"));
 api.use("/teachers", require("./teacher"));
 
